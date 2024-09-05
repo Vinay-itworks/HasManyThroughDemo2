c19a339c9be61abe0d23eeeac8b4c967ba54c29b
Vinay@itworks <vinay.poul@infotech.works>
2024-09-05 17:53:06 +0530

    Created has_many through relationship between Library and Book through Borrow.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  has_many :libraries, through: :borrows
+  has_many :borrows
 end
--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,4 @@
 class Library < ApplicationRecord
+  has_many :books, through: :borrows
+  has_many :borrows
 end
